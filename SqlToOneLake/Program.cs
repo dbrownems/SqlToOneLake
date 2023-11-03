bbf14dfcd1fb78780e091522f23e5643a6b1065a
David Browne <dbrowne@microsoft.com>
2023-11-03 09:52:58 -0500

    refactored code into functions

--- SqlToOneLake/Program.cs
+++ SqlToOneLake/Program.cs
@@ -1,7 +1,4 @@
-﻿
-
-//connect to sql server
-using Azure.Identity;
+﻿using Azure.Identity;
 using Azure.Storage.Files.DataLake;
 using Azure.Storage.Files.DataLake.Models;
 using Microsoft.Data.SqlClient;
@@ -18,106 +15,23 @@ select f.*
 from FactInternetSales f
 cross join generate_series(0,150000000/60368) s
 
-"; 
+";
 var endpopint = $"https://msit-onelake.dfs.fabric.microsoft.com/";
 var workspaceName = "dbrowne_Trident";
 var folder = "/LH.lakehouse/Files/test";
 var filename = "test.parquet";
 var rowGroupSize = 1000000;
 
-var dataLakeServiceClient = GetDataLakeServiceClient(endpopint);
-var dataLakeFileSystemClient = dataLakeServiceClient.GetFileSystemClient(workspaceName);
-var dirClient = dataLakeFileSystemClient.GetDirectoryClient(folder);
-var fileClient = dirClient.GetFileClient(filename);
-using var file = fileClient.OpenWrite(true);
-
+using Stream file = CreateOneLakeFile(endpopint, workspaceName, folder, filename);
 
 using var con = new SqlConnection(constr);
 con.Open();
 var cmd = new SqlCommand(sql, con);
 using var rdr = cmd.ExecuteReader();
 
-var fields = new List<DataField>();
-var columns = new List<DataColumn>();
-var schemaTable = rdr.GetSchemaTable();
+await WriteDatareaderToParquet(rdr, file, rowGroupSize);
 
-for (int i = 0; i < rdr.FieldCount; i++)
-{
-    var field = rdr.GetName(i);
-    var type = rdr.GetFieldType(i);
-
-    var nullable = (bool)schemaTable.Rows[i]["AllowDBNull"];
-
-    var df = new DataField(field, type, isNullable: nullable);
-    fields.Add(df);
-}
-var schema = new ParquetSchema(fields);
-foreach (var df in fields)
-{
-    var dc = new DataColumn(df, Array.CreateInstance(df.ClrNullableIfHasNullsType, rowGroupSize));
-    columns.Add(dc);
-}
-
-var opts = new ParquetOptions();
-var writer = await ParquetWriter.CreateAsync(schema, file, opts, false);
-
-writer.CompressionMethod = CompressionMethod.Snappy;
-
-int rc = 0;
-while (rdr.Read())
-{
-    for (int i = 0; i < rdr.FieldCount; i++)
-    {
-        var value = rdr.GetValue(i);
-        if (value == DBNull.Value)
-            value = null;
-        columns[i].Data.SetValue(value, rc);
-    }
-    rc++;
-    if (rc % rowGroupSize == 0)
-    {
-        
-        Console.WriteLine($"Writing {rc} rows. {DateTime.Now}");
-        using var rgw = writer.CreateRowGroup();
-        foreach (var c in columns)
-        {
-            Console.WriteLine($"Writing {c.Field.Name}");
-            await rgw.WriteColumnAsync(c);
-        }
-        Console.WriteLine($"Completed {rc} rows. {DateTime.Now}");
-        rc =0;
-    }
-}
-if (rc>0)
-{
-    for (int i = 0; i < columns.Count; i++)
-    {
-        var c = columns[i];
-        var nc = new DataColumn(c.Field, Array.CreateInstance(c.Field.ClrNullableIfHasNullsType,rc));
-        columns[i] = nc;
-        for (int rn = 0; rn < rc; rn++)
-        {
-            nc.Data.SetValue(c.Data.GetValue(rn), rn);
-        }
-        
-    }
-
-    Console.WriteLine($"Writing {rc} rows.  Final Rowgroup.");
-    using var rgw = writer.CreateRowGroup();
-    foreach (var c in columns)
-    {
-        Console.WriteLine($"Writing {c.Field.Name}");
-        await rgw.WriteColumnAsync(c);
-    }
-}
-writer.Dispose();
-
-file.Flush();
-file.Close();
 Console.WriteLine($"Complete {DateTime.Now}");
-
-
-
 
 
 
@@ -126,7 +40,98 @@ DataLakeServiceClient GetDataLakeServiceClient(string endpoint)
 {
     DataLakeServiceClient dataLakeServiceClient = new DataLakeServiceClient(
         new Uri(endpoint),
-        new DefaultAzureCredential(new DefaultAzureCredentialOptions() {  ExcludeInteractiveBrowserCredential=false}));
+        new DefaultAzureCredential(new DefaultAzureCredentialOptions() { ExcludeInteractiveBrowserCredential = false }));
 
     return dataLakeServiceClient;
+}
+
+Stream CreateOneLakeFile(string endpopint, string workspaceName, string folder, string filename)
+{
+    var dataLakeServiceClient = GetDataLakeServiceClient(endpopint);
+    var dataLakeFileSystemClient = dataLakeServiceClient.GetFileSystemClient(workspaceName);
+    var dirClient = dataLakeFileSystemClient.GetDirectoryClient(folder);
+    var fileClient = dirClient.GetFileClient(filename);
+    var file = fileClient.OpenWrite(true);
+    return file;
+}
+
+static async Task WriteDatareaderToParquet(SqlDataReader rdr, Stream file,  int rowGroupSize)
+{
+    var fields = new List<DataField>();
+    var columns = new List<DataColumn>();
+    var schemaTable = rdr.GetSchemaTable();
+
+    for (int i = 0; i < rdr.FieldCount; i++)
+    {
+        var field = rdr.GetName(i);
+        var type = rdr.GetFieldType(i);
+
+        var nullable = (bool)schemaTable.Rows[i]["AllowDBNull"];
+
+        var df = new DataField(field, type, isNullable: nullable);
+        fields.Add(df);
+    }
+    var schema = new ParquetSchema(fields);
+    foreach (var df in fields)
+    {
+        var dc = new DataColumn(df, Array.CreateInstance(df.ClrNullableIfHasNullsType, rowGroupSize));
+        columns.Add(dc);
+    }
+
+    var opts = new ParquetOptions();
+    var writer = await ParquetWriter.CreateAsync(schema, file, opts, false);
+
+    writer.CompressionMethod = CompressionMethod.Snappy;
+
+    int rc = 0;
+    while (rdr.Read())
+    {
+        for (int i = 0; i < rdr.FieldCount; i++)
+        {
+            var value = rdr.GetValue(i);
+            if (value == DBNull.Value)
+                value = null;
+            columns[i].Data.SetValue(value, rc);
+        }
+        rc++;
+        if (rc % rowGroupSize == 0)
+        {
+
+            Console.WriteLine($"Writing {rc} rows. {DateTime.Now}");
+            using var rgw = writer.CreateRowGroup();
+            foreach (var c in columns)
+            {
+                Console.WriteLine($"Writing {c.Field.Name}");
+                await rgw.WriteColumnAsync(c);
+            }
+            Console.WriteLine($"Completed {rc} rows. {DateTime.Now}");
+            rc = 0;
+        }
+    }
+    if (rc > 0)
+    {
+        for (int i = 0; i < columns.Count; i++)
+        {
+            var c = columns[i];
+            var nc = new DataColumn(c.Field, Array.CreateInstance(c.Field.ClrNullableIfHasNullsType, rc));
+            columns[i] = nc;
+            for (int rn = 0; rn < rc; rn++)
+            {
+                nc.Data.SetValue(c.Data.GetValue(rn), rn);
+            }
+
+        }
+
+        Console.WriteLine($"Writing {rc} rows.  Final Rowgroup.");
+        using var rgw = writer.CreateRowGroup();
+        foreach (var c in columns)
+        {
+            Console.WriteLine($"Writing {c.Field.Name}");
+            await rgw.WriteColumnAsync(c);
+        }
+    }
+    writer.Dispose();
+
+    file.Flush();
+    file.Close();
 }
